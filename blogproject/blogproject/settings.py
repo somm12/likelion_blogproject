a75aaa2b8a020dbf624514ed567c8fa905cf2a6a
somm12 <thals8744@naver.com>
2021-06-03 12:47:53 +0900

    처음부터

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-e9z!tq%osjqtyp3bnh3*!lax5z7i9cm5=f3luowlrqc@dbw1%p')
+SECRET_KEY = os.environ.get('SECRET_KEY','gdjango-insecure-e9z!tq%osjqtyp3bnh3*!lax5z7i9cm5=f3luowlrqc@dbw1%p')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG', 'True') != 'False')
@@ -79,14 +79,10 @@ WSGI_APPLICATION = 'blogproject.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       'NAME': os.environ.get('DB_NAME'),
-       'USER': os.environ.get('DB_USER'),
-       'PASSWORD': os.environ.get('DB_PASSWORD'),
-       'HOST': os.environ.get('DB_HOST'),
-       'PORT': '',
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
